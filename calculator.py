69b94bc1abaddcbbcac30ede20b5c494a3384cea
Prince Santhosh <105284965+nmhappu@users.noreply.github.com>
2022-08-31 19:02:02 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add():  
+    sum = a + b
+    print(sum,"\n")
+
+def sub():
+    sub = a - b
+    print(sub,"\n")
+
+def mult():
+    mult = a * b
+    print(mult,"\n")
+
+def div():
+    div = a * b
+    print(div,"\n")
+
+func_dict = {'add':add,'sub':sub,"mult":mult,"div":div}
+
+a = int(input("X > "))
+b = int(input("Y > "))
+
+#print(a+b)
+
+call_func = input("op > ")
+func_dict[call_func]()
