652b1a8c53b09d4179747bc786b462efb086c94f
nmhappu <pncsns8194@gmail.com>
2023-02-21 22:07:47 +0530

    example from chapter geometric progressions, record correction, etc

---
+++ geometric_prog ex.py
@@ -0,0 +1,21 @@
+#----request a1,a2 and loop range
+
+s1 = int(input("s1:")) #a1
+s2 = int(input("s2:")) #a2
+rseq = int(input("number of sequences to be printed:"))
+
+x = s1
+y = s2
+
+#----common.ratio
+
+cr = s2 / s1
+cr = int(cr)
+
+print("a1 =", s2,"/",s1)
+print("with common.ratio =",cr)
+
+for i in range(1,rseq+1):
+    x = x * cr
+    y = y * cr
+    print(y,"/",x,)
