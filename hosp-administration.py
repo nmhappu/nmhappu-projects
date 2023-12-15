2414599caf93efe46b6100acd0eaf9e5f29433fa
nmhappu <pncsns8194@gmail.com>
2023-12-16 00:00:57 +0530

    Merged and formatted

--- hosp-administration.py
+++ hosp-administration.py
@@ -18,34 +18,42 @@
 import mysql.connector
 
 print("Welcome To Administrative Block")
-admin = mysql.connector.connect(host="localhost", user="root", passwd="root", database="hospitalm")
-cu=admin.cursor()
+admin = mysql.connector.connect(
+    host="localhost", user="root", passwd="root", database="hospitalm"
+)
+cu = admin.cursor()
 
 if admin.is_connected():
     print("Login Successful!")
 
 while True:
-    print("""1. Add New Doctors
+    print(
+        """1. Add New Doctors
           2.Delete Doctors
-          3. Log Out!""")
-    
+          3. Log Out!"""
+    )
+
     ch = int(input("Enter Your Choice: "))
 
-    if ch==1:
-        name=input("Enter the doctor's name:")
-        gender=input("Enter the doctor's Gender: ")
-        dept=input("Enter the doctor's Department:")
-        exp=input("Enter the doctor's Years of Experience: ")
-        fee=input("Enter the doctor's Consultation fee: ")
-        cu.execute("insert into docs values('{}','{}','{}',{},{})".format(name,gender,dept,exp,fee))
+    if ch == 1:
+        name = input("Enter the doctor's name:")
+        gender = input("Enter the doctor's Gender: ")
+        dept = input("Enter the doctor's Department:")
+        exp = input("Enter the doctor's Years of Experience: ")
+        fee = input("Enter the doctor's Consultation fee: ")
+        cu.execute(
+            "insert into docs values('{}','{}','{}',{},{})".format(
+                name, gender, dept, exp, fee
+            )
+        )
         admin.commit()
         print("Values Added!")
 
-    if ch==2:
-        name=input("Enter the doctor's name you want to delete: ")
+    if ch == 2:
+        name = input("Enter the doctor's name you want to delete: ")
         cu.execute("delete from docs where name='f)'".format(name))
         admin.commit()
         print("Deleted!!")
 
-    if ch==3:
+    if ch == 3:
         exit()
--- hosp-creating_database.py
+++ hosp-creating_database.py
@@ -18,11 +18,17 @@
 
 import mysql.connector
 
-con=mysql.connector.connect(host='localhost', user ='root', passwd='root')
-h_obj=con.cursor
+con = mysql.connector.connect(host="localhost", user="root", passwd="root")
+h_obj = con.cursor
 h_obj.execute("create database hospitalm")
-cons=mysql.connector.connect(host='localhost', user= 'root', passwd='root', database="'hospitalm")
+cons = mysql.connector.connect(
+    host="localhost", user="root", passwd="root", database="'hospitalm"
+)
 h_obj.execute("use hospitalm")
-h_obj.execute("create table users(idno int, Regdat DATE, name char(15),age int, gender char(1))")
-h_obj.execute("create table docs(name char(15), gender char(1), dept char(5), exp int, fee int)")
+h_obj.execute(
+    "create table users(idno int, Regdat DATE, name char(15),age int, gender char(1))"
+)
+h_obj.execute(
+    "create table docs(name char(15), gender char(1), dept char(5), exp int, fee int)"
+)
 print("Database Created.")
