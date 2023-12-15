c7cb235d9989ebcdeca3bbde130f942ed8c9c154
nmhappu <pncsns8194@gmail.com>
2023-12-15 22:33:30 +0530

    test

--- trials.py
+++ trials.py
@@ -1,10 +1,4 @@
 import mysql.connector as mys
 
-mydb = mys.connect(
-  host="localhost",
-  user="root",
-  password="root"
-)
-
-if mys.is_connected():
-    print("aaa")
+def mysql():
+    conn.mysql
