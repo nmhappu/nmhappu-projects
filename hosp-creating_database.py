47879a07b04fb44d5c59524e247a6040a6c8d0f7
nmhappu <pncsns8194@gmail.com>
2023-12-16 00:13:17 +0530

    Renamed vars, changed database name to "hospital_management", formatted everything else for hosp-creating_database.py

--- hosp-creating_database.py
+++ hosp-creating_database.py
@@ -18,17 +18,18 @@
 
 import mysql.connector
 
+# Establishing connection to MySQL server
 con = mysql.connector.connect(host="localhost", user="root", passwd="root")
-h_obj = con.cursor
-h_obj.execute("create database hospitalm")
-cons = mysql.connector.connect(
-    host="localhost", user="root", passwd="root", database="'hospitalm"
-)
-h_obj.execute("use hospitalm")
-h_obj.execute(
-    "create table users(idno int, Regdat DATE, name char(15),age int, gender char(1))"
-)
-h_obj.execute(
-    "create table docs(name char(15), gender char(1), dept char(5), exp int, fee int)"
-)
+
+cursor = con.cursor()
+cursor.execute("CREATE DATABASE hospital_management")
+
+h_con = mysql.connector.connect(
+    host="localhost", user="root", passwd="root", database="hospitalm")
+
+cursor.execute("USE hospitalm")
+
+cursor.execute("CREATE TABLE users(IDNo INT, Regdat DATE, Name CHAR(15), Age INT, Gender CHAR(1))") # Creating a 'users' table
+
+cursor.execute("CREATE TABLE docs(Name CHAR(15), Gender CHAR(1), Dept CHAR(5), Exp INT, Fee INT)") # Creating a 'docs' table
 print("Database Created.")
