ebd82060370be231b6cab4befbfc30708008e4c1
nmhappu <pncsns8194@gmail.com>
2023-12-19 21:51:28 +0530

    updated hosp-management.docx

--- #lab.py
+++
@@ -1,9 +0,0 @@
-'''line = input("enter a line");a = 0;b = 0;c = 0
-for x in line:
-    if x.isupper(): a += 1
-    if x.islower(): b += 1
-    if x.isdigit(): c += 1
-print("Number of uppercase letters","\nNumber of lowercase letters",b,"\nNumber of digits",c)'''
-
-a = {"A":232,"B":3204924}
-print(232 in a)
